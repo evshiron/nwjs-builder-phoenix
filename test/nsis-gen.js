e23ecceed0208c68e79668051cab280134b79e55
evshiron <evshiron@gmail.com>
2017-03-31 23:42:08 +0800

    feat(nsis-gen): add NsisDiffer to make diff scripts for updater

--- test/nsis-gen.js
+++ test/nsis-gen.js
@@ -3,36 +3,51 @@ import { test } from 'ava';
 
 import { writeFileAsync, removeAsync } from 'fs-extra-promise';
 
-import { NsisComposer, nsisBuild } from '../dist/lib/nsis-gen';
+import { NsisComposer, NsisDiffer, nsisBuild } from '../dist/lib/nsis-gen';
 import { tmpName, tmpFile, tmpDir } from '../dist/lib/util';
 
-test('build', async (t) => {
+const options = {
+
+    // Basic.
+    appName: 'Project',
+    companyName: 'evshiron',
+    description: 'description',
+    version: '0.1.0.0',
+    copyright: 'copyright',
+
+    // Compression.
+    compression: 'lzma',
+    solid: true,
+
+};
+
+test.skip('build', async (t) => {
 
     const output = await tmpName();
 
-    const data = await (new NsisComposer({
-
-        // Basic.
-        appName: 'Project',
-        companyName: 'evshiron',
-        description: 'description',
-        version: '0.1.0.0',
-        copyright: 'copyright',
-
-        // Compression.
-        compression: 'lzma',
-        solid: true,
-
-        // Styles.
-        xpStyle: true,
-
-        // Files.
+    const data = await (new NsisComposer(Object.assign({}, options, {
         srcDir: './src/',
-
-        // Output.
         output,
+    })))
+    .make();
 
-    }))
+    const script = await tmpName();
+
+    await writeFileAsync(script, data);
+    await nsisBuild(script);
+
+    await removeAsync(output);
+    await removeAsync(script);
+
+});
+
+test('diff', async (t) => {
+
+    const output = await tmpName();
+
+    const data = await (new NsisDiffer('./src/', './dist/', Object.assign({}, options, {
+        output,
+    })))
     .make();
 
     const script = await tmpName();
