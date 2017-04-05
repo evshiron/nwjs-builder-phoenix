aba14a22616e474b6c060ffc3074d9c792061996
evshiron <evshiron@gmail.com>
2017-04-05 10:40:06 +0800

    refactor(tests): refactor

--- test/Builder.js
+++ test/Builder.js
@@ -6,11 +6,11 @@ import { spawnAsync } from '../dist/lib/util';
 
 const dir = './assets/project/';
 
-test.skip('commandline', async (t) => {
+test('commandline --concurrent', async (t) => {
 
     const mirror = process.env.CI ? '' : '--mirror https://npm.taobao.org/mirrors/nwjs/';
 
-    const { code, signal } = await spawnAsync('node', `./dist/bin/build.js --mac --x64 ${ mirror } --config ${ dir }/package.json ${ dir }`.split(' '), {
+    const { code, signal } = await spawnAsync('node', `./dist/bin/build.js --win --mac --linux --x64 --concurrent ${ mirror } ${ dir }`.split(' '), {
         stdio: 'inherit',
     });
     t.is(code, 0);
@@ -25,24 +25,6 @@ test.skip('module', async (t) => {
         win: true,
         x64: true,
         mirror,
-    }, dir);
-
-    await builder.build();
-
-});
-
-test('concurrent', async (t) => {
-
-    const mirror = process.env.CI ? undefined : 'https://npm.taobao.org/mirrors/nwjs/';
-
-    const builder = new Builder({
-        win: true,
-        mac: true,
-        linux: true,
-        x64: true,
-        mirror,
-        concurrent: true,
-        mute: false,
     }, dir);
 
     await builder.build();
