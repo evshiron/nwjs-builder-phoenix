434aea44a0bac3f75a7b9f24db167436952370b1
evshiron <evshiron@gmail.com>
2017-08-21 14:53:05 +0800

    fix(Downloaders): tolerate DNS lookup timeout and use caches

--- src/lib/Downloader.ts
+++ src/lib/Downloader.ts
@@ -67,12 +67,21 @@ export class Downloader extends DownloaderBase {
             }
         }
         catch(err) {
+
+            debug('in fetch', 'err', err);
+
             if(err.code === 'ENOTFOUND' && this.options.useCaches) {
+                console.info('DNS lookup fails, use local caches at this time.');
+                return path;
+            }
+            else if(err.code === 'EAI_AGAIN' && this.options.useCaches) {
+                console.info('DNS lookup timeout, use local caches at this time.');
                 return path;
             }
             else {
                 throw err;
             }
+
         }
 
         await this.download(url, filename, path, showProgress);
